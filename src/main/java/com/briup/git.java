bb8d8ddb118cb903db96765ee789c8f0f961fb60
wyl <sh15234081518@163.com>
2022-08-14 18:22:26 +0800

    push commit

--- src/main/java/com/briup/git.java
+++ src/main/java/com/briup/git.java
@@ -9,5 +9,6 @@ public class git {
         System.out.println("git test5");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push test");
     }
 }
