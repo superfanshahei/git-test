996cef87d4c6c728c62e7d707e1a005ceb07c4ea
wyl <sh15234081518@163.com>
2022-08-14 17:27:56 +0800

    second git test

--- src/main/java/com/briup/git.java
+++ src/main/java/com/briup/git.java
@@ -3,5 +3,7 @@ package com.briup;
 public class git {
     public static void main(String[] args) {
         System.out.println("git test");
+        System.out.println("git test1");
+        System.out.println("git test2");
     }
 }
